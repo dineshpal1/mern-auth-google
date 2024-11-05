c41b776d85c263441cc5290851d30e5e2512661b
dinesh <dineshpaldinesh3@gmail.com>
2024-11-05 16:55:46 +0530

    installed routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/Signin";
+import SignUp from "./pages/Signup";
+import Profile from "./pages/Profile";
 
-
-const App = () => {
+export default function App() {
   return (
-    <div className="text-red-500">Hello</div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
 
-export default App
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
