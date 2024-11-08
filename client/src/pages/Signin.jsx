271dced049aa10f607d936efd883d709214cccfc
dinesh <dineshpaldinesh3@gmail.com>
2024-11-08 14:32:59 +0530

    add rtk

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react';
 import { Link,useNavigate  } from 'react-router-dom';
-
-
+import { signInStart,signInSuccess,signInFailure } from '../redux/user/userSlice';
+import { useDispatch, useSelector } from 'react-redux';
 export default function Signin() {
   const [formData, setFormData] = useState({});
-  const [error,setError]=useState(false)
-  const [loading,setLoading]=useState(false)
+  // const [error,setError]=useState(false)
+  // const [loading,setLoading]=useState(false)
+  const {loading,error}=useSelector((state)=>state.user)
+  // console.log(loading,error)
   const navigate=useNavigate();
+  const dispatch=useDispatch();
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
@@ -15,8 +18,9 @@ export default function Signin() {
     e.preventDefault();
       
      try {
-      setLoading(true)
-      setError(false)
+      // setLoading(true)
+      // setError(false)
+      dispatch(signInStart())
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: {
@@ -26,16 +30,20 @@ export default function Signin() {
       });
       const data = await res.json();
       // console.log(data);
-      setLoading(false)
+      // setLoading(false)
+      
       if(data.success===false){
-        setError(true)
+        // setError(true)
+        dispatch(signInFailure(data.message))
         return
       }
-      setError(false)
+      // setError(false)
+      dispatch(signInSuccess(data))
       navigate("/")
      } catch (error) {
-      setLoading(false)
-      setError(true)
+      // setLoading(false)
+      // setError(true)
+      dispatch(signInFailure(error))
      }
     } 
   
@@ -79,7 +87,8 @@ export default function Signin() {
           <span className='text-blue-500'>Sign Up</span>
         </Link>
       </div>
-      <p className='text-red-700 mt-5'>{error && "Something went wrong"}</p>
+      <p className='text-red-700 mt-5'>
+        {error?error || "Something went wrong":""}</p>
     </div>
   );
 }
